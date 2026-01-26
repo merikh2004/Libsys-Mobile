39296a4fcdf177eb85321c61d92139cc4c6e6c11
Joshua <merikh2004>
2026-01-26 17:37:42 +0800

    Chore: Remove Comment

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  // Dito natin sasabihin na lahat ng files sa app at src folders ay may tailwind
   content: ["./App.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
